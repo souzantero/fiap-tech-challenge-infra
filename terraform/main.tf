3d6bd63799b6ced118268b019398cec5e8c60926
Felipe Antero <souzantero@gmail.com>
2023-10-26 22:12:40 -0300

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,97 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name                 = "fiap-tech-challenge"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    PROJECT = "fiap-tech-challenge"
+    STAGE   = "dev"
+  }
+}
+
+resource "aws_db_subnet_group" "fiap_tech_challenge" {
+  name       = "fiap-tech-challenge"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    PROJECT = "fiap-tech-challenge"
+    STAGE   = "dev"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name   = "fiap-tech-challenge-rds"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    PROJECT = "fiap-tech-challenge"
+    STAGE   = "dev"
+  }
+}
+
+resource "aws_db_parameter_group" "fiap_tech_challenge" {
+  name   = "fiap-tech-challenge"
+  family = "postgres13"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
+resource "aws_db_instance" "fiap_tech_challenge" {
+  identifier             = "fiap-tech-challenge"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "13.7"
+  username               = "fiap_tech_challenge"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.fiap_tech_challenge.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.fiap_tech_challenge.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+
+  tags = {
+    PROJECT = "fiap-tech-challenge"
+    STAGE   = "dev"
+  }
+}

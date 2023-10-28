096604676f890032edb7c1f30187e1b619539594
Felipe Antero <souzantero@gmail.com>
2023-10-28 19:02:08 -0300

    configure eks

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.fiap_tech_challenge.address
@@ -14,4 +19,19 @@ output "rds_username" {
   description = "RDS instance root username"
   value       = aws_db_instance.fiap_tech_challenge.username
   sensitive   = true
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
 }

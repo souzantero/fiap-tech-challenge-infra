fe0d5f08026ae4c240cecc4fb3a430a5beffa09b
Felipe Antero <souzantero@gmail.com>
2023-10-30 23:38:37 -0300

    chore(terraform): format main

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,13 +33,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
 
-  name                 = "fiap-tech-challenge"
+  name = "fiap-tech-challenge"
 
-  cidr                 = "10.0.0.0/16"
-  azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true

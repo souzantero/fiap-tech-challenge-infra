3b082721ef1bf671148769434c1f4ea9158fab16
Felipe Antero <souzantero@gmail.com>
2023-10-30 21:16:53 -0300

    change outputs sensitive

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,19 +6,16 @@ output "region" {
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.fiap_tech_challenge.address
-  sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
   value       = aws_db_instance.fiap_tech_challenge.port
-  sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
   value       = aws_db_instance.fiap_tech_challenge.username
-  sensitive   = true
 }
 
 output "cluster_endpoint" {
